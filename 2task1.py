4ec0279d3559b2373a1740905eebab0a5ac32bda
abikrish <abirami.krishnaraj@dal.ca>
2018-10-04 17:58:37 -0700

    commit

--- 2task1.py
+++ 2task1.py
@@ -1,5 +1,4 @@
 #Comment
-from collections import Counter
 file=open("Crime.csv")
 count=Counter()
 dict={}
@@ -9,15 +8,18 @@ l3=[] #list3
 for line in file:
 	line=line.strip()
 	line=line.split(",")
-	l1.append(line[-1]) #listing all crime type in l1 
-	l2.append(line[-2]) #listing all crime id inl2
-	for i in l1:
+	l1.append(line[-1]) #listing all crime type in l1
+	l2.append(line[-2]) #listing all crime id in l2
+for i in l1:
 		if i not in dict:
 			dict[i]=1
 		else:
 			dict[i]+=1
-print(dict)
-
-	#count[i]+=1
+print([dict])
+#test=zip(dict,l2)
+#for xy in test:
+#	print(xy)
+#print(l2[0:])
+		#count[i]+=1
 	#print(dict,l2)
 
