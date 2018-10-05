fd00b9e0fe931c24b19cb59ed63dcac53ee1b46f
abikrish <abirami.krishnaraj@dal.ca>
2018-10-04 18:41:26 -0700

    commit

--- 2task1.py
+++ 2task1.py
@@ -1,24 +1,31 @@
 #Comment
 file=open("Crime.csv")
 dict={}
+dict2={}
 l1=[] #list1
 l2=[] #list2
-l3=[] #list3
 for line in file:
 	line=line.strip()
 	line=line.split(",")
 	l1.append(line[-1]) #listing all crime type in l1
 	l2.append(line[-2]) #listing all crime id in l2
+"""Counting crime name and crime count"""
 for i in l1:
-		if i not in dict:
-			dict[i]=1
-		else:
-			dict[i]+=1
+		if i not in dict: #crime type-i not in l1
+			dict[i]=1 #append the element i
+		else:		  #else
+			dict[i]+=1 #add count
 print(dict)
-print(l2)
-"""test=zip(dict,l2)
+"""Counting Crime ID"""
+for y in l2:
+		if y not in dict2: #crime ID not in l2
+			dict2[y]=1 #append the element y
+		else:		   #else
+			dict2[y]+=1 #add the count
+#print(dict2)
+"""zipping Crime name, crime  ID"""
+test=zip(dict,dict2)
 for xy in test:
-	print(xy)"""
-#count[i]+=1
-#print(dict,l2)
+	print(xy)
+
 
